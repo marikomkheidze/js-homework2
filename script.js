75abe159aaa689a82eac9cf70255775b85fb7f7f
marikomkheidze <110020576+marikomkheidze@users.noreply.github.com>
2022-09-06 12:43:16 +0400

    5-6

---
+++ script.js
@@ -0,0 +1,50 @@
+// დავალება 1
+
+
+
+
+
+
+
+
+// დავალება 2 
+
+
+
+
+
+
+
+// დავალება 3
+
+
+
+
+
+// დავალება 4
+
+
+
+
+
+// დავალება 5 
+// let array = [1,2,4,10,34,5,7,87];
+// for (let i=0; i <array.length; i++) {
+//     const element = array [i]
+//     if (10 > element > 0)
+//     console.log(element);
+// }
+
+// დავალება 6
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let i = 0; i < numbers.length; i++) {
+//     const element = numbers[i];
+//     if (element == 5) {
+//         break
+//     }
+//     console.log(element);
+// }
+
+
+
